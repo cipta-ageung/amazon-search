df1a054f4f7e5d6844d7d391b0dd45587fba9ebe
ciptaageung <cipta_ageung@yahoo.co.id>
2020-04-02 13:20:48 +0700

    update proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,7 @@
 const proxy = require('http-proxy-middleware');
 module.exports = function expressMiddleware(app) {
   app.use(
-    proxy(['/admin', '/login', '/product'], {
+    proxy(['/'], {
       target: process.env.REACT_APP_API_URL,
       changeOrigin: true
     })
